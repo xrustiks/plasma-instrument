2d99d33707802fac46f060bf669a45501356c63a
shaliruna <shaliruna4@gmail.com>
2026-04-18 01:45:04 +0200

    refactored

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 import cors from 'cors';
 import express from 'express';
-import { API_PREFIX } from './config/constants.js';
+import { API_PREFIX, PORT } from './config/constants.js';
 import apiRoutes from './routes/router.js';
 
 const app = express();
@@ -10,4 +10,16 @@ app.use(express.json());
 
 app.use(API_PREFIX, apiRoutes);
 
-export default app;
+function main() {
+	try {
+		app.listen(PORT, () => {
+			console.log(`API server running on http://localhost:${PORT}`);
+			console.log('Admin panel: http://localhost:8000/admin/');
+		});
+	} catch (error) {
+		console.error('Server error:', error);
+		process.exit(1);
+	}
+}
+
+main();
